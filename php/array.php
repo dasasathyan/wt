644fb973b879503e534cb8c0cf19b376a18bd0e7
dasasathyan@github.com <dasasathyan@gmail.com>
2016-09-23 10:46:20 +0530

    wt full

--- php/array.php
+++ php/array.php
@@ -1,5 +1,5 @@
 <?php
-$das1=["1st line 1st array"=>"keys","1st line 2nd array","1st line 3rd  array"];
+$das1=["1st line 1st array"=>"keys","ajith"=>"1st line 2nd array","1st line 3rd  array"];
 $das2 = array("2nd line 1st","2nd line 2nd","2nd line 3rd");
 $das3= array("great"=>array("3rd line 1st","3rd line 2nd","3rd line 3rd"),array(4,5,6));
  print_r($das1);
--- php/cookies.php
+++ php/cookies.php
@@ -1,14 +1,18 @@
 <?php
-$user="Dasa Sathyan";
+
+//$user="Dasa Sathyan";
+if(isset($_REQUEST['sub'])){
+echo "going in";
+$user=$_REQUEST['user'];
 setcookie("user", $user, time() + (5), "/");
+echo "welcome ". $_COOKIE["user"];
+}
 ?>
 <html>
 <body>
-
-<?php
-    //echo $name;
-    echo "welcome ". $_COOKIE["user"];
-?>
-
+<form method="post">
+<input type="text" name="user"/>
+<input type="submit" name="sub"/>
+</form>
 </body>
 </html>
